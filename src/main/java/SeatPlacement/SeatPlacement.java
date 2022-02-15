effbdbe07421157287c3b70ef3d88e1ac2e105fd
Aykoda <aykodas@gmail.com>
2022-02-15 20:03:42 +0200

    V.1.3 SeatPlacement evaluating

--- src/main/java/SeatPlacement/SeatPlacement.java
+++ src/main/java/SeatPlacement/SeatPlacement.java
@@ -1,9 +1,6 @@
 package SeatPlacement;
 
-import java.util.Map;
-
 public class SeatPlacement {
-
     public enum seatsOrdering{
         BY_LINEAR_SELECT,
         BY_CUSTOM_SELECT,
@@ -11,51 +8,96 @@ public class SeatPlacement {
         BY_BEST_PRIORITY,
         BY_RESTRICTIONS;
     }
-
+    private boolean isCapacityFull;
     public final seatsOrdering DEFAULT_ORDER = seatsOrdering.BY_LINEAR_SELECT;
     private seatsOrdering seatsOrder;
-    private Map<Integer,Integer> nextEmptySeatPlace;
+    private int[] nextEmptySeat = new int[2];
 
-    AreaSeatsImpl AreaSeatsPlacement;
-    UsersTicketsImpl UsersTickets;
+    AreaSeats AreaSeatsPlacement;
+    UsersTickets UsersTickets;
 
-    public SeatPlacement(){}
-
-    public SeatPlacement(AreaSeatsImpl AreaSeatsPlacement, UsersTicketsImpl UsersTickets){
+    public SeatPlacement(AreaSeats AreaSeatsPlacement, UsersTickets UsersTickets){
       this.AreaSeatsPlacement = AreaSeatsPlacement;
       this.UsersTickets = UsersTickets;
 
       setSeatsOrder(DEFAULT_ORDER);
+      setCapacityFull(IsCapacityFull());
     }
 
-    public void EvaluateSeatPlacement(seatsOrdering orderingCase){
-        setSeatsOrder(orderingCase);
-        switch (getSeatsOrder()) {
-            case BY_LINEAR_SELECT:
-                EvaluateLinearOrdering();
-                break;
-            case BY_CUSTOM_SELECT:
-                break;
-            case BY_RANDOM_SELECT:
-                break;
+    public void EvaluateSeatPlacement(){//seatsOrdering orderingCase){
+        if (getCapacityFull() ==false) { //setSeatsOrder(orderingCase);
+            switch (getSeatsOrder()) {
+                case BY_LINEAR_SELECT:
+                    EvaluateLinearOrdering();
+                    break;
+                case BY_CUSTOM_SELECT:
+                    break;
+                case BY_RANDOM_SELECT:
+                    break;
+            }
         }
+        else
+            System.out.println("Area Full Capacity.");
+            //throw new Exception("Area Full Capacity.");//System.out.println("");//new Exception();
     }
     private void EvaluateLinearOrdering(){
 
+        System.out.println("---- Ticket number:"+UsersTickets.getTicketsNumber());
+        int[] nextEmptySeat = new int[2];
+        int r=getNextEmptySeat()[0];
+        int c=getNextEmptySeat()[1];
+        Integer areaSeats[][] = AreaSeatsPlacement.getAreaSeats();
+        int i;
+        for (i=0; i<UsersTickets.getTicketsNumber(); i++) {
+            areaSeats[r][c] = 1;
+            System.out.printf("Ticket:%s r:%s c:%s value:%s\n",i+1,r+1,c+1,areaSeats[r][c]);
+
+            if (isLastSeatAtRow(c) == true) {
+                if (isLastRow(r) == true) { //IsCapacityFull()
+                    i++;
+                    setCapacityFull(true);
+                    break;
+                }
+                else {
+                    r += 1;
+                    c = 0;
+                }
+            }else
+                c += 1;
+        }
+        AreaSeatsPlacement.setAllocatedSeats(i);
+        AreaSeatsPlacement.setAreaSeats(areaSeats);
+        setNextEmptySeat(new int[]{r,c});
+    }
+    public boolean getCapacityFull() {
+        return isCapacityFull;
     }
 
-
-    public Map<Integer,Integer> getNextEmptySeatPlace() {
-        return nextEmptySeatPlace;
+    public void setCapacityFull(boolean capacityFull) {
+        isCapacityFull = capacityFull;
     }
 
-    public void setNextEmptySeatPlace(Map<Integer,Integer> nextSeatPlace) {
-        this.nextEmptySeatPlace = nextSeatPlace;
+    public boolean IsCapacityFull(){
+        int row =getNextEmptySeat()[0] + 1;
+        int col =getNextEmptySeat()[1] + 1;
+        return (row*col == AreaSeatsPlacement.getAreaCapacity())?
+                true :false;
     }
 
-    public int isLastRowSeats(){
-        int nextColumn=0;
-        return nextColumn;
+    public int[] getNextEmptySeat() {
+        return nextEmptySeat;
+    }
+
+    public void setNextEmptySeat(int[] nextEmptySeat) {
+        this.nextEmptySeat = nextEmptySeat;
+    }
+
+    public boolean isLastSeatAtRow(int lastColumn){
+        return (lastColumn == AreaSeatsPlacement.getColumnSeats()-1) ? true:false;
+    }
+
+    public boolean isLastRow(int lastRow){
+        return (lastRow == AreaSeatsPlacement.getRowSeats()-1) ? true:false;
     }
 
     public seatsOrdering getSeatsOrder() {
